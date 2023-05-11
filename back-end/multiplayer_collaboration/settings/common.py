bb224e9170af59ba027514b86902da06b08b456a
Mariam <mari.kifshidze@gmail.com>
2023-05-11 13:10:20 +0400

    Add new apps to INSTALLED_APPS

--- back-end/multiplayer_collaboration/settings/common.py
+++ back-end/multiplayer_collaboration/settings/common.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # --Custom Apps
+    'user.apps.UserConfig',
+    'project.apps.StoreConfig',
+
+    # -- Packages
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
