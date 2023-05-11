c0f4d7962b0d6d3666581be7611b3c4cbc3108d9
Mariam <mari.kifshidze@gmail.com>
2023-05-11 19:49:30 +0400

    Create ProjectDetail RetrieveAPIView

--- back-end/project/urls.py
+++ back-end/project/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from project import views
+from project.views import ProjectDetail
 
 router = DefaultRouter()
-router.register('project', views.ProjectViewSet, basename="project")
+router.register('projects', views.ProjectViewSet, basename="project")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path(r'project_detail/<int:pk>/', ProjectDetail.as_view()),
 ]
--- back-end/project/views.py
+++ back-end/project/views.py
@@ -1,3 +1,6 @@
+from rest_framework import generics
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from project.models import Project
@@ -7,3 +10,12 @@ from project.serializers import ProjectSerializer
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class ProjectDetail(generics.RetrieveAPIView):
+    queryset = Project.objects.all()
+    renderer_classes = [TemplateHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        project = self.get_object()
+        return Response({'project': project}, template_name='project_detail.html')
