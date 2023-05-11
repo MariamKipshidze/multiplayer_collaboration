87ff68b51c61944fe49050ab060f272a64a40329
Mariam <mari.kifshidze@gmail.com>
2023-05-11 16:34:05 +0400

    Create Project model

--- back-end/project/models.py
+++ back-end/project/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=255)
+    image = models.ImageField(verbose_name=_("image"), upload_to='project/projects', blank=True, null=True)
+    description = models.TextField(verbose_name=_("Description"), null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Project')
+        verbose_name_plural = _('Projects')
+        ordering = ['pk']
