fe9672c9bf8efe656c8c425e62faaebf7bd4fcee
Mariam <mari.kifshidze@gmail.com>
2023-05-11 18:40:24 +0400

    Fixed url

--- back-end/multiplayer_collaboration/urls.py
+++ back-end/multiplayer_collaboration/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include('project.urls')),
+    path('project/', include('project.urls')),
 ]
 
 if settings.DEBUG:
