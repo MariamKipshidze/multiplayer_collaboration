aa3d84db06c83268df6384825b95a8472433ea74
Mariam <mari.kifshidze@gmail.com>
2023-05-18 16:29:43 +0400

    Add project routing to ProtocolTypeRouter

--- back-end/multiplayer_collaboration/routing.py
+++ back-end/multiplayer_collaboration/routing.py
@@ -1,10 +1,12 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+import project.routing
+
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            # project routing
+            project.routing.websocket_urlpatterns
         )
     ),
 })
