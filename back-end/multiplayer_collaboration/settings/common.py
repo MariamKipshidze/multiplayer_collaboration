308e166bfa967658d6425c18fcd51cf9aa6cb240
Mariam <mari.kifshidze@gmail.com>
2023-05-22 11:37:36 +0400

    Add CACHES to settings file

--- back-end/multiplayer_collaboration/settings/common.py
+++ back-end/multiplayer_collaboration/settings/common.py
@@ -149,3 +149,16 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels.layers.InMemoryChannelLayer'
     },
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+    },
+    "alternate": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+    }
+}
