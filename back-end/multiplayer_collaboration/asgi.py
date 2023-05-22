9abcab94f44f05d952424848def9f88f066e94f3
Mariam <mari.kifshidze@gmail.com>
2023-05-22 13:19:51 +0400

    Update asgi file

--- back-end/multiplayer_collaboration/asgi.py
+++ back-end/multiplayer_collaboration/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import project.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multiplayer_collaboration.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
